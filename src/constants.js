f2b9bb2e4545e325086d648992404c3b92503341
jens1101 <jens1101@users.noreply.github.com>
2020-11-18 16:24:31 +0000

    Deploying to gh-pages from  @ 9cdb72c1d1700b6082f987a41f3c2dd5075d4c65 🚀

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-export const STATUS = {
-  CONFIRMED: "confirmed",
-  RECOVERED: "recovered",
-  DEATHS: "deaths",
-};
-
-export const WINDOW_SIZE = 10;
-
-export const API_BASE = "https://api.covid19api.com";
